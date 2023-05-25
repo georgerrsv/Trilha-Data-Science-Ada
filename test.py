da16bdd2d4a79000822b44d8746a5b0cc3c9b835
George <georgerrsilva@alu.ufc.br>
2023-05-25 19:36:10 -0300

    python file test

---
+++ test.py
@@ -0,0 +1,12 @@
+def verifica_par():
+    x = int(input("Digite o primeiro número: "))
+    y = int(input("Digite o segundo número: "))
+
+    if ((x + y)% 2) == 0:
+        print("O resultado é =", x+y)
+        print("A soma dos números é par.")
+    else:
+        print("O resultado é =", x+y) 
+        print("A soma dos números é ímpar.")
+
+verifica_par()
